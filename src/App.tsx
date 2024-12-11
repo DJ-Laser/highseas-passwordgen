6d851e6be7c60ea4887cc29809b8d7cae7a7a3b7
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-10 22:57:44 -0500

    make papar bg image stretch

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { PasswordGenerator } from "./components/PasswordGenerator";
 function App() {
   return (
     <div
-      className="p-10 bg-cover"
+      className="p-10 bg-[size:100%_100%]"
       style={{
         backgroundImage: `url('${map}')`,
       }}
     >
-      <h1 className="my-8 font-bold text-parchment-ink-dark text-5xl">
+      <h1 className="mt-6 mb-10 font-bold text-parchment-ink-dark text-5xl">
         Password Generator
       </h1>
       <PasswordGenerator />
