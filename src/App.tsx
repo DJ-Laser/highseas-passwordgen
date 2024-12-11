82b2c4a12fffac70f0d043fc3edcf94f2d100b6b
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-10 22:07:07 -0500

    add image (no 9 slicing bc border image and svg wont play nice :( )

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import map from "./assets/map-parchment.svg";
 import { PasswordGenerator } from "./components/PasswordGenerator";
 
 function App() {
   return (
-    <>
+    <div
+      className="p-8 bg-cover"
+      style={{
+        backgroundImage: `url('${map}')`,
+      }}
+    >
       <h1 className="my-8 font-bold text-5xl">Password Generator</h1>
       <PasswordGenerator />
-    </>
+    </div>
   );
 }
 
